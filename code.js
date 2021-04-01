324a244e886b26e2ef5abdf13ff63d1b5ff82000
J Parrott <j@Js-MacBook-Pro.attlocal.net>
2021-04-01 11:40:12 -0400

    We finished the add disc function

--- code.js
+++ code.js
@@ -9,11 +9,23 @@ const boardModel = [
     [null, null, null, null, null, null, null],
 ];
 
-let player = null;
+let player = 1;
 
 function addDiscToBoard() { //Medium difficulty
-    // STUB
-}
+    let slotPlace = document.getElementsByTagName("td")
+    // Create a for loop tht loops thru the td
+    for (let i = 0; i < slotPlace.length; i++) {
+        slotPlace[i].addEventListener("click", (event) => {
+            console.log(
+                `${event.target.parentElement.rowIndex}, ${event.target.cellIndex}}`
+            )
+// line 18 - 20 code was inspire by Krystal Briggs
+        })
+
+        }
+    }
+console.log(addDiscToBoard())
+
 
 // Win condition
 // Needs to know what slot a disc is played in, and what player did it. (parameters: slot/location, player)
@@ -87,23 +99,20 @@ console.log(checkForWin())
 
 // When a player clicks empty slot, disc appears. Then program alternates to the other player.
 function changePlayer() {
-    if (player === null) {
-        player = null;
-    } else if (player === null) {
-        player = null;
+    if (player === 1) {
+        player = 1;
+    } else if (player === 2) {
+        player = 2;
     }
     // TO-DO: Render on the page to say whose turn it is.
 }
 
 function handleGame() { // Medium
-    // STUB
+// this function is another add event listener function that renders everything unto the page
+    
+    
 }
 
-// Create an event listener function that recognize a mouse click
-// function slotClick("click", callback) {
-//   const slot = getElementsByClassName(slot) 
-//   document
-// }
 
 function reset() {
     
